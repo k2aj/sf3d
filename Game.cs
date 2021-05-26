ae18b3150cf099be99b17b733a9651f58ee3dd19
Krzysztof Jajeśnica <krzysztof.jajesnica@gmail.com>
2021-05-26 19:25:06 +0200

    Fixed mismatched shader attribute types in example program

--- Game.cs
+++ Game.cs
@@ -46,9 +46,9 @@ namespace SF3D
             vShader = new(ShaderType.VertexShader, @"
                 #version 330 core
                 layout(location=0) in vec2 position;
-                layout(location=1) in vec3 color;
+                layout(location=1) in vec4 color;
 
-                out vec3 vColor;
+                out vec4 vColor;
 
                 void main() {
                     gl_Position = vec4(position, 0.5, 1.0);
